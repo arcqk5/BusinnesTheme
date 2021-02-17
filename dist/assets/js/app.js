4cba4af367b11ad01e1e147447c3c2d67d341d56
arcqk5 <grigoriylafutkin@yandex.ru>
2021-02-17 20:26:21 +0300

    Changed index.html

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -3044,3 +3044,27 @@ $(function() {
 
 
 });
+
+var navigation = document.getElementById('navigation');
+var burger = document.getElementById('burgerButton');
+
+burger.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    navigation.classList.toggle('show');
+});
+
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute('href');
+
+        navigation.classList.remove('show');
+
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+};
